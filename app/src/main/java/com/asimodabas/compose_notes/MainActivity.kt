c8f361e971951519e5a1961f163c75447a046aef
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-01-28 03:35:57 +0300

    modifier

--- app/src/main/java/com/asimodabas/compose_notes/MainActivity.kt
+++ app/src/main/java/com/asimodabas/compose_notes/MainActivity.kt
@@ -3,16 +3,18 @@ package com.asimodabas.compose_notes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.asimodabas.compose_notes.ui.theme.ComposeNotesTheme
 
@@ -30,16 +32,18 @@ class MainActivity : ComponentActivity() {
         //Column
         Column {
             Text(
+                modifier = Modifier
+                    .background(color = Color.Red)
+                    .padding(5.dp),
                 text = "Text 1",
                 color = Color.Black,
                 fontSize = 15.sp,
                 fontWeight = FontWeight.Bold)
-            Text(
-                text = "Text 2",
-                color = Color.Gray,
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Medium)
+
+            CustomText(text = "Custom Text")
         }
+
+    /*
 
         //Row
         Row {
@@ -68,6 +72,25 @@ class MainActivity : ComponentActivity() {
                 fontSize = 15.sp,
                 fontWeight = FontWeight.Medium)
         }
+
+    */
+    }
+
+    @Composable
+    fun CustomText(text:String){
+
+        Text(
+            modifier = Modifier
+                .background(color = Color.Black)
+                .padding(5.dp)
+                .clickable {
+                    println("Clicked")
+                },
+            text = text,
+            color = Color.Red,
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Medium)
+
     }
 
     @Preview(showBackground = true)
