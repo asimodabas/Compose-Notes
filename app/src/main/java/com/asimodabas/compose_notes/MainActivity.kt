70bbc1f22dd8745b4f8cc6062aa116427ac98713
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-01-28 22:23:15 +0300

    compose

--- app/src/main/java/com/asimodabas/compose_notes/MainActivity.kt
+++ app/src/main/java/com/asimodabas/compose_notes/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,67 +33,105 @@ class MainActivity : ComponentActivity() {
     fun mainScreen() {
 
         //Column
-        Column {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.DarkGray),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
             Text(
                 modifier = Modifier
                     .background(color = Color.Red)
                     .padding(5.dp),
-                text = "Text 1",
+                text = "Text",
                 color = Color.Black,
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Bold)
-
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.padding(5.dp))
             CustomText(text = "Custom Text")
+            Spacer(modifier = Modifier.padding(5.dp))
+            Row {
+                Text( modifier = Modifier
+                    .background(Color.Red)
+                    .padding(5.dp),
+                    text = "Text 1",
+                    color = Color.Black,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.padding(5.dp))
+                CustomText(text = "Text 2")
+                Spacer(modifier = Modifier.padding(5.dp))
+                Text(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .padding(5.dp),
+                    text = "Text 3",
+                    color = Color.Gray,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Medium
+                )
+                }
+            Spacer(modifier = Modifier.padding(5.dp))
+            Button(onClick = {
+                println("Clicked Button")
+            }){
+                Text(text = "Button")
+            }
         }
 
-    /*
+        /*
 
-        //Row
-        Row {
-            Text(
-                text = "Text 1",
-                color = Color.Black,
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Bold)
-            Text(
-                text = "Text 2",
-                color = Color.Gray,
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Medium)
-        }
+            //Row
+            Row {
+                Text(
+                    text = "Text 1",
+                    color = Color.Black,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Text 2",
+                    color = Color.Gray,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Medium)
+            }
 
-        //Box
-        Box() {
-            Text(
-                text = "Text 1",
-                color = Color.Black,
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Bold)
-            Text(
-                text = "Text 2",
-                color = Color.Gray,
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Medium)
-        }
+            //Box
+            Box() {
+                Text(
+                    text = "Text 1",
+                    color = Color.Black,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Text 2",
+                    color = Color.Gray,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Medium)
+            }
 
-    */
+        */
     }
 
     @Composable
-    fun CustomText(text:String){
+    fun CustomText(text: String) {
 
         Text(
             modifier = Modifier
-                .background(color = Color.Black)
-                .padding(5.dp)
                 .clickable {
                     println("Clicked")
-                },
+                }
+                .background(color = Color.Black)
+                .padding(5.dp),
             text = text,
             color = Color.Red,
-            fontSize = 15.sp,
-            fontWeight = FontWeight.Medium)
-
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
     }
 
     @Preview(showBackground = true)
