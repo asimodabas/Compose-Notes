4ee7c14cc95ffe0d44b74726610b7b41d1517d96
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-06-10 22:30:12 +0300

    Countdown flow added

--- app/src/main/java/com/asimodabas/compose_notes/MainActivity.kt
+++ app/src/main/java/com/asimodabas/compose_notes/MainActivity.kt
@@ -3,19 +3,20 @@ package com.asimodabas.compose_notes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,8 +33,20 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ComposeNotesTheme {
+
+                val viewModel : FlowViewModel by viewModels()
+                val counter = viewModel.countDownTimerFlow.collectAsState(initial = 10)
+
+                Surface(color = MaterialTheme.colors.background) {
+                    Box(modifier = Modifier.fillMaxSize()){
+                        Text(text = counter.value.toString(), fontSize = 20.sp, textAlign = TextAlign.Center, modifier = Modifier.align(
+                            Alignment.Center))
+                    }
+
+                }
                 //mainScreen()
             }
         }
